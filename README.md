049c98c0df37c0ee830e423f6aa3c43bc5718ddb
Eugene Apollonsky <e.apollonsky@gmail.com>
2019-03-02 19:14:11 +0200

    optimized version

--- README.md
+++ README.md
@@ -3,9 +3,6 @@ go build anagram.go
 
 Anagrams:
 
-bin/anagram ~/Downloads/lemmad.txt maja
-290.078235ms ajam, jaam, jama, maja
-
 bin/anagram ~/Downloads/lemmad.txt katus
-304.958142ms aktus, katus, kaust, kutsa, skaut, tasku
+42.426898ms aktus, katus, kaust, kutsa, skaut, tasku
 ```
--- anagram.go
+++ anagram.go
@@ -2,86 +2,22 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"reflect"
+	"runtime"
 	"sort"
 	"strings"
+	"sync"
 	"time"
-	"unicode"
 )
 
-type Rep struct {
-	Char rune
-	N    int8
-}
-
-type Dict struct {
-	Index map[Rep]*Dict
-	Words []string
-}
-
-func findAnagrams(d *Dict, reps []Rep) []string {
-	if len(reps) == 0 {
-		return d.Words
-	}
-	if d, ok := d.Index[reps[0]]; ok {
-		return findAnagrams(d, reps[1:])
-	}
-	return nil
-}
-
-func parseWord(word string) []Rep {
-	chars := []rune(word)
-	sort.Slice(chars, func(i, j int) bool {
-		return chars[i] < chars[j]
-	})
-	var reps []Rep
-	for _, c := range chars {
-		c = unicode.ToLower(c)
-		if len(reps) > 0 && reps[len(reps)-1].Char == c {
-			reps[len(reps)-1].N++
-		} else {
-			reps = append(reps, Rep{Char: c, N: 1})
-		}
-	}
-	return reps
-}
-
-func insertWord(d *Dict, reps []Rep, w string) {
-	if len(reps) == 0 {
-		d.Words = append(d.Words, w)
-	} else {
-		var newd *Dict
-		if newd = d.Index[reps[0]]; newd == nil {
-			newd = &Dict{Index: make(map[Rep]*Dict)}
-			d.Index[reps[0]] = newd
-		}
-		insertWord(newd, reps[1:], w)
-	}
-}
-
-func indexWord(d *Dict, w string) {
-	insertWord(d, parseWord(w), w)
-}
-
-func parseDict(words []string) Dict {
-	d := Dict{Index: make(map[Rep]*Dict)}
-	for _, w := range words {
-		indexWord(&d, w)
-	}
-	return d
-}
-
-func readDict(path string) ([]string, error) {
-	b, err := ioutil.ReadFile(path)
+func readDict(path string, wordc chan string) error {
+	f, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	scan := bufio.NewScanner(bytes.NewReader(b))
-	var words []string
+	defer f.Close()
+	scan := bufio.NewScanner(f)
 	for scan.Scan() {
 		// hack to encode Latin1 to UTF8
 		bytes := scan.Bytes()
@@ -89,9 +25,60 @@ func readDict(path string) ([]string, error) {
 		for i, b := range bytes {
 			runes[i] = rune(b)
 		}
-		words = append(words, string(runes))
+		wordc <- string(runes)
 	}
-	return words, nil
+	close(wordc)
+	return nil
+}
+
+func getThreadsNum() int {
+	nthreads := runtime.NumCPU()/2 - 1
+	if nthreads < 1 {
+		return 1
+	}
+	return nthreads
+}
+
+func sortedRunes(w string) []rune {
+	runes := []rune(strings.ToLower(w))
+	sort.Slice(runes, func(i, j int) bool {
+		return runes[i] < runes[j]
+	})
+	return runes
+}
+
+func anagrams(path, word string) ([]string, error) {
+	var (
+		wordc    = make(chan string, 1024)
+		srunes   = sortedRunes(word)
+		nthreads = getThreadsNum()
+		wg       sync.WaitGroup
+
+		mu       sync.Mutex
+		anagrams []string
+	)
+	for n := 0; n < nthreads; n++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for word := range wordc {
+				if string(srunes) == string(sortedRunes(word)) {
+					mu.Lock()
+					anagrams = append(anagrams, word)
+					mu.Unlock()
+				}
+
+			}
+		}()
+	}
+
+	err := readDict(path, wordc)
+	if err != nil {
+		return nil, err
+	}
+
+	wg.Wait()
+	return anagrams, nil
 }
 
 func main() {
@@ -102,20 +89,11 @@ func main() {
 	}
 	dictPath := os.Args[1]
 	word := strings.Join(os.Args[2:], " ")
-	words, err := readDict(dictPath)
+
+	ans, err := anagrams(dictPath, word)
 	if err != nil {
 		fmt.Println("Failed to read a dictionary:", err)
 		return
 	}
-
-	reps := parseWord(word)
-	var anagrams []string
-	for _, w := range words {
-		if reflect.DeepEqual(reps, parseWord(w)) {
-			anagrams = append(anagrams, w)
-		}
-	}
-	// d := parseDict(words)
-	// anagrams := findAnagrams(&d, parseWord(word))
-	fmt.Println(time.Since(start), strings.Join(anagrams, ", "))
+	fmt.Println(time.Since(start), strings.Join(ans, ", "))
 }
